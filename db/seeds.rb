a0a4b015f50d08fe36d35c716e13557a870fcac6
caseykung17 <caseykung17@tamu.edu>
2024-12-31 14:52:27 +0800

    small syntax changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,19 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
   {:title => 'Spirited Away', :rating => 'PG',
-    :release_date => '20-Dec-2001'}
+    :release_date => '20-Dec-2001'},
   {:title => 'Dune: Part One', :rating => 'PG-13',
-    :release_date => '22-Oct-2021'}
+    :release_date => '22-Oct-2021'},
   {:title => 'Howl\'s Moving Castle', :rating => 'PG',
-    :release_date => '24-Feb-2005'}
+    :release_date => '24-Feb-2005'},
   {:title => 'Kingsman: The Secret Service', :rating => 'M18',
     :release_date => '12-Feb-2015'}
 ]
 
 more_movies.each do |movie|
-  Movie.find_or_create_by(title: movie[:title], release_date: movie[:release_date]) do |diff_movie|
-    diff_movie.rating = movie[:rating]
+  Movie.find_or_create_by(title: movie[:title], release_date: movie[:release_date])
+    do |diff_movie| diff_movie.rating = movie[:rating]
   end
 end
